0f30b6fdc5b04fa559fb021d5093a8af56a870ce
banahaker <pn0818x@outlook.com>
2024-02-03 16:51:27 +0800

    feat: _Tensor.new_full()

--- src/tensor/_tensor/_tensor.zig
+++ src/tensor/_tensor/_tensor.zig
@@ -14,7 +14,7 @@ pub fn _Tensor(comptime Type: type) type {
 
         const Self = @This();
 
-        pub fn init(shape: []const usize, requires_grad: bool, data: ?[]Type) !Self {
+        pub fn init(shape: []const usize, data: ?[]const Type, requires_grad: bool) !Self {
             var size: usize = 1;
             for (shape) |dim_size| {
                 size *= dim_size;
@@ -76,8 +76,29 @@ pub fn _Tensor(comptime Type: type) type {
             return self.dim;
         }
 
-        pub fn new_tensor(self: Self, shape: usize, data: []type, require_grad: bool) _Tensor(type) {
-            return _Tensor(self).init(shape, require_grad, data);
+        pub fn print(self: Self) !void {
+            for (self.T) |t| {
+                std.debug.print("{d} ", .{t});
+            }
+            std.debug.print("\n", .{});
+        }
+
+        pub fn new_tensor(self: Self, shape: []const usize, data: []const Type, require_grad: bool) !_Tensor(Type) {
+            _ = self;
+            return _Tensor(Type).init(shape, data, require_grad);
+        }
+
+        pub fn new_full(self: Self, shape: []const usize, data: Type, require_grad: bool) !_Tensor(Type) {
+            _ = self;
+            var size: usize = 1;
+            for (shape) |dim_size| {
+                size *= dim_size;
+            }
+            const tdata = try std.heap.page_allocator.alloc(Type, size);
+            for (0..tdata.len) |i| {
+                tdata[i] = data;
+            }
+            return _Tensor(Type).init(shape, tdata, require_grad);
         }
     };
 }
@@ -85,13 +106,13 @@ pub fn _Tensor(comptime Type: type) type {
 // Test
 test "init" {
     const f32Tensor = _Tensor(f32);
-    var tensor: f32Tensor = try f32Tensor.init(&[_]usize{ 3, 3, 3 }, false, null);
+    var tensor: f32Tensor = try f32Tensor.init(&[_]usize{ 3, 3, 3 }, null, false);
     _ = tensor;
 }
 
 test "set and get" {
     const f32Tensor = _Tensor(f32);
-    var tensor: f32Tensor = try f32Tensor.init(&[_]usize{ 3, 3, 3 }, false, null);
+    var tensor: f32Tensor = try f32Tensor.init(&[_]usize{ 3, 3, 3 }, null, false);
 
     try tensor.set(&[_]usize{ 1, 2, 0 }, 3.14);
     const result = try tensor.get(&[_]usize{ 1, 2, 0 });
@@ -107,6 +128,20 @@ test "set and get" {
 
 test "get dimemsion" {
     const f32Tensor = _Tensor(f32);
-    var tensor = try f32Tensor.init(&[_]usize{ 3, 3, 3 }, false, null);
+    var tensor: f32Tensor = try f32Tensor.init(&[_]usize{ 3, 3, 3 }, null, false);
     try std.testing.expect(tensor.getDim() == 3);
+}
+
+test "new_tensor" {
+    const f32Tensor = _Tensor(f32);
+    var tensor: f32Tensor = try f32Tensor.init(&[_]usize{ 3, 3, 3 }, null, false);
+    var new_tensor = try tensor.new_tensor(&[_]usize{3}, &[_]f32{ 1, 2, 3 }, false);
+    try new_tensor.print();
+}
+
+test "new_full" {
+    const f32Tensor = _Tensor(f32);
+    var tensor: f32Tensor = try f32Tensor.init(&[_]usize{ 3, 3, 3 }, null, false);
+    var new_tensor = try tensor.new_full(&[_]usize{3}, 3, false);
+    try new_tensor.print();
 }
